33f6cc21f25ff591fde387620450b7405064b4b4
Scott Robertson <the.scott.rob@gmail.com>
2016-04-16 18:24:52 -0400

    added updated sim

--- road_rage_simulation.py
+++ road_rage_simulation.py
@@ -1,13 +1,13 @@
 import random
-
+from collections import deque
 from road_rage_car import *
+import matplotlib.pyplot as plt
 
 class Simulation:
     def __init__(self):
         self.cars = []
-        total_cars = range(3)
+        total_cars = range(30)
         car_in_front = None
-
         for i in total_cars:
             car = Car(i * 20, car_in_front)
             car_in_front = car
@@ -16,16 +16,36 @@ class Simulation:
         self.cars[0].car_in_front = self.cars[-1]
 
     def run(self):
-        while True:
-            for car in self.cars:
-                # for 10% of the time...
-                car.acceleration = random.randint(-2, 2)
+        count = 0
+        count_car = 30
+
+        list_for_graph = []
+        while count <= 10:
+            for car in reversed(self.cars):
+                print('-------------->car: ', count_car)
+
                 if car.can_move():
+                    if random.random() < .1:
+                        car.slow()
+                    else:
+                        car.accel()
                     car.move()
-            break # only run 1 time
+
+                if count_car == 1:
+                    count_car = 30
+                else:
+                    count_car -= 1
+
+                list_for_graph.append(car.pos_in_road)
+            count += 1
+        return list_for_graph
+
 
 def main():
-    Simulation().run()
+    listy = Simulation().run()
+    plt.plot(listy, 'ro')
+    #plt.axis([-1, 11, -1, 1000])
+    plt.show()
 
 if __name__ == '__main__':
     main()
